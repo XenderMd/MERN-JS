7add07c3f72480a85ffebea70a577d34bbae6da1
XenderMd <denis.stavilamd@gmail.com>
2020-09-27 18:54:45 +0200

    Fetching User Input

--- React Refresher/src/App.js
+++ React Refresher/src/App.js
@@ -13,7 +13,8 @@ const App = () => {
 ], );
 
 const addNewGoalHandler = (newGoal)=>{
-  setCourseGoals(courseGoals.concat(newGoal))
+  //setCourseGoals(courseGoals.concat(newGoal))
+  setCourseGoals((prevCourseGoals)=>{return prevCourseGoals.concat(newGoal)});
 };
 
   return( 
--- React Refresher/src/components/NewGoal/NewGoal.js
+++ React Refresher/src/components/NewGoal/NewGoal.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import "./NewGoal.css"
 
 const NewGoal = (props)=>{
+
+    const [enteredText, setEnteredText]=useState();
 
     const addGoalHandler = (event) =>{
         event.preventDefault();
 
         const newGoal={
             id: Math.random().toString(),
-            text:"dummy text"
+            text: enteredText
         };
 
         props.onAddGoal(newGoal);
+        setEnteredText('');
+    };
+
+    const textChangeHandler = (event)=>{
+        setEnteredText(event.target.value);
     };
 
     return(
     <form className='new-goal' onSubmit={addGoalHandler}>
-        <input type="text"></input>
+        <input type="text" value={enteredText} onChange={textChangeHandler}></input>
         <button type="submit">Add Goal</button>
     </form>
     )
