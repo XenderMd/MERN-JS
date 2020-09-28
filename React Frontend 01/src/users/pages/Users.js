51cc83a1539408fe8fa681c8ef2f07ed6bba1ffc
XenderMD <denis.stavilamd@gmail.com>
2020-09-28 19:08:43 +0200

    Adding a UserList page and components

--- React Frontend 01/src/users/pages/Users.js
+++ React Frontend 01/src/users/pages/Users.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = ()=>{
-    return <h2>Users Works !!</h2>
+    const USERS = [{id:'u1', name:"Sensei", image:"https://geekculture.co/wp-content/uploads/2018/03/karate-kid-sequel-cobra-kai-johnny-featured-886x500.jpg", places:3}];
+
+    return <UsersList items={USERS}/>
 };
 
 export default Users;
