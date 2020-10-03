c0e042ae9e1bc6174ad3a2e8c3eda9c2a7c2eb5f
XenderMD <denis.stavilamd@gmail.com>
2020-10-03 19:05:17 +0200

    Rendering User Places and Using Dynamic Route Segments

--- React Frontend 01/src/App.js
+++ React Frontend 01/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom
 import Users from './users/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import UserPlaces from './places/pages/UserPlaces';
 
 
 const App = ()=>{
@@ -17,6 +18,9 @@ const App = ()=>{
         </Route>
         <Route path="/places/new">
             <NewPlace/>
+        </Route>
+        <Route path="/:userId/places" exact>
+            <UserPlaces></UserPlaces>
         </Route>
         <Redirect to="/"></Redirect>
       </Switch>
