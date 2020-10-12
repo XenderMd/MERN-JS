787af511ae76b536d03a1aa4b8b1d7649a69489d
XenderMD <denis.stavilamd@gmail.com>
2020-10-12 15:40:27 +0200

    Sharing Input Values & Adding multiple inputs

--- React Frontend 01/src/shared/components/FormElements/Input.js
+++ React Frontend 01/src/shared/components/FormElements/Input.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 
 import { validate } from "../../util/validators";
 
@@ -23,11 +23,19 @@ const inputReducer = (state, action) => {
 };
 
 const Input = (props) => {
+    
   const [inputState, dispatch] = useReducer(inputReducer, {
     value: "",
     isValid: false,
     isTouched:false
   });
+
+  const {id, onInput}= props;
+  const {value, isValid}=inputState;
+
+  useEffect(()=>{
+    onInput(id, value, isValid)
+  }, [id, value, isValid, onInput]);
 
   const changeHandler = (event) => {
     dispatch({
