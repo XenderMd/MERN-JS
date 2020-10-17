e202085acc0fbb4ece9f3b9b4940bd18ded07182
XenderMD <denis.stavilamd@gmail.com>
2020-10-17 19:11:38 +0200

    Basic Node.js server, wich services a simple POST request

---
+++ Express Refresher/app.js
@@ -0,0 +1,31 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("Incoming Request");
+  console.log(req.method, req.url);
+
+  if (req.method === 'POST') {
+      let body='';
+
+      req.on('end', ()=>{
+
+        const userName=body.split('=')[1];
+
+        console.log(body);
+
+        res.end("<h1>"+userName+"</h1>");
+    });
+
+      req.on('data', (chunk)=>{
+         body+=chunk
+      });
+
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.end(
+      '<form method="POST"><input type="text" name="username"></input><button>Create User</button></form>'
+    );
+  }
+});
+
+server.listen(5000);
