c30bdcc5992a3fc6588b94cba10572a5ac9cb888
XenderMD <denis.stavilamd@gmail.com>
2020-10-17 18:01:03 +0200

    Added Auth Context to the EDIT and DELETE button in the PlaceItem component

--- React Frontend 01/src/places/components/PlaceItem.js
+++ React Frontend 01/src/places/components/PlaceItem.js
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
 import Modal from "../../shared/components/UIElements/Modal";
 import Map from "../../shared/components/UIElements/Map";
+import {AuthContext} from '../../shared/context/auth-context';
+
 
 import "./PlaceItem.css";
+import Auth from "../../users/pages/Auth";
 
 const PlaceItem = (props) => {
+
+  const auth=useContext(AuthContext);
   const [showMap, setShowMap] = useState(false);
   const [showConfirmModal, setShowConfirmModal]=useState(false);
 
@@ -72,8 +77,8 @@ const PlaceItem = (props) => {
             <Button onClick={openMapHandler} inverse>
               VIEW ON MAP
             </Button>
-            <Button to={`/places/${props.id}`}>EDIT</Button>
-            <Button danger onClick={showDeleteWarningHandler}>DELETE</Button>
+            {auth.isLoggedIn &&<Button to={`/places/${props.id}`}>EDIT</Button>}
+            {auth.isLoggedIn && <Button danger onClick={showDeleteWarningHandler}>DELETE</Button>}
           </div>
         </Card>
       </li>
