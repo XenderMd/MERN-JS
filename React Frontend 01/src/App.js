6242b8d4bba4b520d993c67ab667baf85f290ad8
XenderMD <denis.stavilamd@gmail.com>
2020-10-17 17:13:43 +0200

    Added Auth context for App-wide State Management

--- React Frontend 01/src/App.js
+++ React Frontend 01/src/App.js
@@ -1,40 +1,59 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
+import React, {useState, useCallback} from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
-import Users from './users/pages/Users';
-import Auth from './users/pages/Auth';
-import NewPlace from './places/pages/NewPlace';
-import UpdatePlace from './places/pages/UpdatePlace';
-import MainNavigation from './shared/components/Navigation/MainNavigation';
-import UserPlaces from './places/pages/UserPlaces';
+import { AuthContext } from "./shared/context/auth-context";
+import Users from "./users/pages/Users";
+import Auth from "./users/pages/Auth";
+import NewPlace from "./places/pages/NewPlace";
+import UpdatePlace from "./places/pages/UpdatePlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UserPlaces from "./places/pages/UserPlaces";
+
+const App = () => {
+
+  const [isLoggedIn, setIsLoggedIn]=useState(false);
+  
+  const login= useCallback(()=>{
+      setIsLoggedIn(true);
+  },[]);
+
+  const logout= useCallback(()=>{
+    setIsLoggedIn(false);
+},[]);
 
 
-const App = ()=>{
   return (
-    <Router>
-      <MainNavigation/>
-      <main>
-      <Switch>
-        <Route path="/" exact>
-            <Users/>
-        </Route>
-        <Route path="/auth" exact>
-            <Auth/>
-        </Route>
-        <Route path="/places/new">
-            <NewPlace/>
-        </Route>
-        <Route path="/places/:placeId">
-            <UpdatePlace/>
-        </Route>
-        <Route path="/:userId/places" exact>
-            <UserPlaces></UserPlaces>
-        </Route>
-        <Redirect to="/"></Redirect>
-      </Switch>
-      </main>
-    </Router>
-  )
+    <AuthContext.Provider value={{isLoggedIn:isLoggedIn, login:login, logout:logout}}>
+      <Router>
+        <MainNavigation />
+        <main>
+          <Switch>
+            <Route path="/" exact>
+              <Users />
+            </Route>
+            <Route path="/auth" exact>
+              <Auth />
+            </Route>
+            <Route path="/places/new">
+              <NewPlace />
+            </Route>
+            <Route path="/places/:placeId">
+              <UpdatePlace />
+            </Route>
+            <Route path="/:userId/places" exact>
+              <UserPlaces></UserPlaces>
+            </Route>
+            <Redirect to="/"></Redirect>
+          </Switch>
+        </main>
+      </Router>
+    </AuthContext.Provider>
+  );
 };
 
 export default App;
