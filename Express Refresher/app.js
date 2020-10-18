c286f3e3a8f7af6bf7c9e2cbec0ec62974f714b9
XenderMD <denis.stavilamd@gmail.com>
2020-10-18 21:05:46 +0200

    Built a basic Express server with url bodyparser and some middleware for the post and get requests

--- Express Refresher/app.js
+++ Express Refresher/app.js
@@ -1,32 +1,18 @@
 const express = require("express");
-//const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  let body = "";
-  req.on("end", () => {
-    const userName = body.split("=")[1];
-    if (userName) {
-      req.body = { name: userName };
-    }
-    next();
-  });
-  req.on("data", (chunk) => {
-    body += chunk;
-  });
+app.post('/user', (req, res, next) => {
+  res.send(`<h1> ${req.body.username} is cool </h1>`);
 });
 
-app.use((req, res, next) => {
-  if (req.body) {
-    res.send(`<h1> ${req.body.name} is cool </h1>`);
-  } else {
-    res.send(
-      '<form method="POST"><input type="text" name="username"></input><button type="submit">Create User</button></form>'
-    );
-  }
+app.get('/', (req, res, next) => {
+  res.send(
+    '<form action ="/user" method="POST"><input type="text" name="username"></input><button type="submit">Create User</button></form>'
+  );
 });
 
 app.listen(5000);
