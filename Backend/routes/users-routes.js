e3bdf927df532aa3ca36fadead22b35c40f6bbce
XenderMD <denis.stavilamd@gmail.com>
2020-11-30 14:09:44 +0100

    Using multer to save files (images) on the backend

--- Backend/routes/users-routes.js
+++ Backend/routes/users-routes.js
@@ -1,14 +1,28 @@
 const express = require("express");
-const {check} = require("express-validator");
+const { check } = require("express-validator");
 
-const userControllers= require('../controllers/users-controllers');
+const userControllers = require("../controllers/users-controllers");
+const fileUpload = require("../middleware/file-upload");
 
 const router = express.Router();
 
-router.get('/', userControllers.getUsers);
+router.get("/", userControllers.getUsers);
 
-router.post('/signup',[check('name').not().isEmpty(), check('email').isEmail(), check('password').isLength({min:6})], userControllers.userSignup);
+router.post(
+  "/signup",
+  fileUpload.single("image"),
+  [
+    check("name").not().isEmpty(),
+    check("email").isEmail(),
+    check("password").isLength({ min: 6 }),
+  ],
+  userControllers.userSignup
+);
 
-router.post('/login',[check('email').isEmail(), check('password').not().isEmpty()], userControllers.userLogin);
+router.post(
+  "/login",
+  [check("email").isEmail(), check("password").not().isEmpty()],
+  userControllers.userLogin
+);
 
 module.exports = router;
